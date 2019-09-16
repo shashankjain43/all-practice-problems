3eeab218a9e93fabd56486b45a8c70ff8c047e16
shashank jain <shashank.jain43@gmail.com>
2019-09-16 17:02:25 +0530

    Added spring boot project for practice purpose

---
+++ all-coding-problems/src/main/java/com/practice/allcodingproblems/AllCodingProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.allcodingproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllCodingProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllCodingProblemsApplication.class, args);
+	}
+
+}
