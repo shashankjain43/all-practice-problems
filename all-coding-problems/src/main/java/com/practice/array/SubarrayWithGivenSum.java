63ecf525feec06be6209c52f22cfcdbefd5533f9
Shashank Jain <shajain8@publicisgroupe.net>
2019-09-17 08:42:54 +0530

    Array problem 1

---
+++ all-coding-problems/src/main/java/com/practice/array/SubarrayWithGivenSum.java
@@ -0,0 +1,93 @@
+package com.practice.array;
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class SubarrayWithGivenSum {
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int test = fr.nextInt();
+        for (int i = 0; i < test; i++) {
+            int size = fr.nextInt();
+            int sum = fr.nextInt();
+            int arr[] = new int[size];
+            for (int j = 0; j < size; j++) {
+                arr[j] = fr.nextInt();
+            }
+            subarrayWithGivenSum(arr, sum);
+        }
+    }
+
+    private static void subarrayWithGivenSum(int[] arr, int sum) {
+
+        int currSum = arr[0];
+        int lIndex = 0;
+        int rIndex = -1;
+        for (int i = 1; i < arr.length; i++) {
+
+            currSum = currSum + arr[i];
+
+            while (currSum > sum) {
+                currSum = currSum - arr[lIndex];
+                lIndex++;
+            }
+
+            if (currSum == sum) {
+                rIndex = i;
+                break;
+            }
+        }
+
+        if (rIndex > -1) {
+            System.out.println(String.format("%s %s", ++lIndex, ++rIndex));
+        } else
+            System.out.println("" + -1);
+
+
+    }
+}
