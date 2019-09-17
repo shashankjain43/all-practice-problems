715464f8a8d658285d18fdf05fba7127560f3b85
Shashank Jain <shajain8@publicisgroupe.net>
2019-09-17 11:34:23 +0530

    Subarray with max sum

---
+++ all-coding-problems/src/main/java/com/practice/array/SubarrayWithMaxSum.java
@@ -0,0 +1,39 @@
+package com.practice.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SubarrayWithMaxSum {
+
+    public static void main(String[] args) {
+
+        FastReader fr = new FastReader();
+        int test = fr.nextInt();
+        for (int i = 0; i < test; i++) {
+            int size = fr.nextInt();
+            int arr[] = new int[size];
+            for (int j = 0; j < size; j++) {
+                arr[j] = fr.nextInt();
+            }
+            printSubarrayWithMaxSum(arr);
+        }
+    }
+
+    private static void printSubarrayWithMaxSum(int[] arr) {
+
+        int globalMax = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currSum = currSum + arr[i];
+            if(currSum > globalMax){
+                globalMax = currSum;
+            }
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        System.out.println(globalMax);
+    }
+}
