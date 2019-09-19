298cf41a0bc06a2f4ea7843c3b4d6fdf7b206fe5
shashank jain <shashank.jain43@gmail.com>
2019-09-19 19:39:04 +0530

    EquilibriumPoint

---
+++ all-coding-problems/src/main/java/com/practice/array/EquilibriumPoint.java
@@ -0,0 +1,34 @@
+package com.practice.array;
+
+import java.util.Arrays;
+
+public class EquilibriumPoint {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int test = fr.nextInt();
+        for (int i = 0; i < test; i++) {
+            int size = fr.nextInt();
+            int arr[] = new int[size];
+            for (int j = 0; j < size; j++) {
+                arr[j] = fr.nextInt();
+            }
+            System.out.println(getEquilibriumPoint(arr));
+        }
+    }
+
+    private static int getEquilibriumPoint(int[] arr) {
+        int eqIndex = -1;
+        int rSum = Arrays.stream(arr).sum();
+        int lSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            rSum = rSum - arr[i];
+            if (rSum == lSum) {
+                eqIndex = i + 1;
+                break;
+            }
+            lSum = lSum + arr[i];
+        }
+        return eqIndex > -1 ? eqIndex : -1;
+    }
+}
