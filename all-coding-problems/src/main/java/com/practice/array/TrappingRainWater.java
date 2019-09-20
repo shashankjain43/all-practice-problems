e7a12c6837f312ff93e059a90a3fad8693a7f451
Shashank Jain <shajain8@publicisgroupe.net>
2019-09-20 13:41:10 +0530

    TrappingRainWater

--- all-coding-problems/src/main/java/com/practice/array/TrappingRainWater.java
+++ all-coding-problems/src/main/java/com/practice/array/TrappingRainWater.java
@@ -3,45 +3,46 @@ package com.practice.array;
 public class TrappingRainWater {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int arr[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-		System.out.println("Water trapped is: " + getTrappedWater(arr));
-
+		FastReader fr = new FastReader();
+		int test = fr.nextInt();
+		for (int i = 0; i < test; i++) {
+			int size = fr.nextInt();
+			int arr[] = new int[size];
+			for (int j = 0; j < size; j++) {
+				arr[j] = fr.nextInt();
+			}
+			System.out.println(getTrappedWater(arr));
+		}
 	}
 
-	private static int getTrappedWater(int[] arr) {
-		// TODO Auto-generated method stub
-		int water = 0;
-		int size = arr.length;
-		int currMaxFromLeft = arr[0];
-		int maxFromLeft[] = new int[size];
-		maxFromLeft[0] = currMaxFromLeft;
-		
-		int currMaxFromRight = arr[size - 1];
-		int maxFromRight[] = new int[size];
-		maxFromRight[size - 1] = currMaxFromRight;
-		for (int i = 1; i < size; i++) {
-			if (currMaxFromLeft > arr[i]) {
-				maxFromLeft[i] = currMaxFromLeft;
-			} else {
-				currMaxFromLeft = arr[i];
-				maxFromLeft[i] = arr[i];
-			}
-			
-			if (currMaxFromRight > arr[size - i]) {
-				maxFromRight[size - i] = currMaxFromRight;
-			} else {
-				currMaxFromRight = arr[size - i];
-				maxFromRight[size - i] = arr[size - i];
-			}
-		}
-		for (int i = 0; i < size; i++) {
-			if(Math.min(maxFromLeft[i], maxFromRight[i]) > arr[i]) {
-				water = water + Math.abs(Math.min(maxFromLeft[i], maxFromRight[i])-arr[i]);
-			}
-		}
-		return water;
-	}
+    private static int getTrappedWater(int[] arr) {
+        int water = 0;
+        int size = arr.length;
+        int currMaxFromLeft = Integer.MIN_VALUE;
+        int currMaxFromRight = Integer.MIN_VALUE;
+        int maxFromLeft[] = new int[size];
+        int maxFromRight[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > currMaxFromLeft) {
+                maxFromLeft[i] = arr[i];
+                currMaxFromLeft = arr[i];
+            } else {
+                maxFromLeft[i] = currMaxFromLeft;
+            }
+            if (arr[size - i - 1] > currMaxFromRight) {
+                maxFromRight[size - i - 1] = arr[size - i - 1];
+                currMaxFromRight = arr[size - i - 1];
+            } else {
+                maxFromRight[size - i - 1] = currMaxFromRight;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (Math.min(maxFromLeft[i], maxFromRight[i]) > arr[i]) {
+                water = water + Math.abs(Math.min(maxFromLeft[i], maxFromRight[i]) - arr[i]);
+            }
+        }
+        return water;
+    }
 
 }
