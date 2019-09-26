1c8c0275fa03b22766c3d1033775098a01dc6e80
shashank jain <shashank.jain43@gmail.com>
2019-09-26 12:42:26 +0530

    LongestDistinctCharsInString

--- all-coding-problems/src/main/java/com/practice/gfg/string/LongestDistinctCharsInString.java
+++ all-coding-problems/src/main/java/com/practice/gfg/string/LongestDistinctCharsInString.java
@@ -2,58 +2,10 @@ package com.practice.gfg.string;
 
 import com.practice.gfg.FastReader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 public class LongestDistinctCharsInString {
-
-    static class FastReader {
-
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        public double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        public String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-    }
 
     public static void main(String[] args) {
         FastReader fr = new FastReader();
