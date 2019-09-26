0577b83f23b9fe3838f756372b6e30ca65f4f403
shashank jain <shashank.jain43@gmail.com>
2019-09-26 13:11:26 +0530

    LongestCommonPrefix

--- all-coding-problems/src/main/java/com/practice/gfg/array/LongestCommonPrefix.java
+++ all-coding-problems/src/main/java/com/practice/gfg/array/LongestCommonPrefix.java
@@ -4,20 +4,19 @@ import com.practice.gfg.FastReader;
 
 public class LongestCommonPrefix {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		FastReader fastReader = new FastReader();
-		int testCases = fastReader.nextInt();
-		for (int test = 0; test < testCases; test++) {
-			int noOfWords = fastReader.nextInt();
-			Trie trie = new Trie();
-			for (int j = 0; j < noOfWords; j++) {
-				String word = fastReader.next();
-				trie.insert(word);
-			}
-			System.out.println(trie.getLongestCommonPrefix());
-
-		}
-	}
-
+        FastReader fastReader = new FastReader();
+        int testCases = fastReader.nextInt();
+        for (int test = 0; test < testCases; test++) {
+            int noOfWords = fastReader.nextInt();
+            Trie trie = new Trie();
+            for (int j = 0; j < noOfWords; j++) {
+                String word = fastReader.next();
+                trie.insert(word);
+            }
+            String result = trie.getLongestCommonPrefix();
+            System.out.println(result.isEmpty() ? -1 : result);
+        }
+    }
 }
