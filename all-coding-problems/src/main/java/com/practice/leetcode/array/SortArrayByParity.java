785ee504dba0afce2a3d58fc1da7149c9485f340
shashank jain <shashank.jain43@gmail.com>
2019-10-14 20:38:24 +0530

    SortArrayByParity

--- all-coding-problems/src/main/java/com/practice/leetcode/array/SortArrayByParity.java
+++ all-coding-problems/src/main/java/com/practice/leetcode/array/SortArrayByParity.java
@@ -27,10 +27,45 @@ public class SortArrayByParity {
         return A;
     }
 
+    public int[] sortArrayByParity1(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            //till even on left
+            while (left < arr.length && arr[left] % 2 == 0) {
+                left++;
+            }
+            //till odd on right
+            while (right >= 0 && arr[right] % 2 != 0) {
+                right--;
+            }
+            if(left<right){
+                swap(arr, left, right);
+            }
+            left++;
+            right--;
+        }
+        return arr;
+
+    }
+
+
+    private void swap(int[] a, int left, int right) {
+        if (left < a.length && right >= 0) {
+            int temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+        }
+
+    }
+
     public static void main(String[] args) {
         SortArrayByParity obj = new SortArrayByParity();
-        int[] arr = {3, 1, 2, 4};
-        obj.sortArrayByParity(arr);
+        int[] arr = {0, 2};
+        obj.sortArrayByParity1(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
