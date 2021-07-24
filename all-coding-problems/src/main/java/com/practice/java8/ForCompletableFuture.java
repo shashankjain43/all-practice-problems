d16b37fea8938c61137f7760e7cd7393a4246816
Shashank Jain <shashank.jain@telekom-digital.com>
2021-07-24 15:22:43 +0530

    ForCompletableFuture

--- all-coding-problems/src/main/java/com/practice/java8/ForCompletableFuture.java
+++ all-coding-problems/src/main/java/com/practice/java8/ForCompletableFuture.java
@@ -1,20 +1,46 @@
 package com.practice.java8;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 public class ForCompletableFuture {
 
     public static void main(String[] args) {
-        createCF().thenAccept(integer -> System.out.println(integer))
-                .thenAccept(aVoid -> Syste ,m.out.println("Void"))
-                 xzas767\0;-0ijm.thenR
-uis dkdl l'   cvlkp4ee '
-        wrdfdldcc
+
+        /*createCF().thenAccept(integer -> System.out.println(integer))
+                .thenAccept(aVoid -> System.out.println("Void"));*/
+
+        ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+                                System.out.println("Thread: " + Thread.currentThread());
+                                return 11;
+                            }, forkJoinPool);
+        completableFuture.thenApplyAsync(integer -> {
+                                System.out.println("Thread: " + Thread.currentThread());
+                                return integer * 10;
+                            })
+                        .thenApplyAsync(integer -> {
+                                System.out.println("Thread: " + Thread.currentThread());
+                                return integer + 2;
+                            })
+                        .thenAcceptAsync(integer -> {
+                            System.out.println("Thread: " + Thread.currentThread());
+                            System.out.println(integer);
+                        });
+
+
+        System.out.println("Built the pipeline");
+
+        completableFuture.complete(16);
+
+        completableFuture.thenAccept(integer -> {
+            System.out.println("Thread: "+ Thread.currentThread());
+            System.out.println(integer);
+        });
 
     }
 
     private static CompletableFuture<Integer> createCF() {
         return CompletableFuture.supplyAsync(() -> 2);
     }
-dc
+
 }
